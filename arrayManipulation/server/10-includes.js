5b503633ae001889f4c7dae9fa1cc90eee73d83b
Ruben <rubenvegajusto@gmail.com>
2023-11-17 17:29:44 +0000

    includes

---
+++ arrayManipulation/server/10-includes.js
@@ -0,0 +1,17 @@
+const pets = ['cat', 'dog', 'bat'];
+
+let includeInArray = false;
+for (let index = 0; index < pets.length; index ++) {
+    const element = pets[index];
+    if (element === 'dog') {
+        includeInArray = true;
+        break;
+    }
+}
+
+
+//
+
+const rta = pets.includes(item => item === 'dog')
+console.log('for', includeInArray);
+console.log('includes', rta);
